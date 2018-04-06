a8de4827b17a554a6a25c7242a69efb0e1f49fc4
Dave Garwacke <dave.garwacke@warbyparker.com>
2018-04-06 13:45:46 -0400

    Build prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   entry: { main: ["./index.js"] },
+  output: {
+    filename: process.env.NODE_ENV === "production" ? "prod.js" : "dev.js",
+  },
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
   module: {
     rules: [
